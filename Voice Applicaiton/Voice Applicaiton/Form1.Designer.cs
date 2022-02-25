02dd1b012b3f77e29d74d1381eebf0f3e253b610
budurmete <budurmete@gmail.com>
2022-02-25 19:41:57 +0300

    V:0.1

---
+++ Voice Applicaiton/Voice Applicaiton/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace VoiceApplicaion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKONUS = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblKONUS
+            // 
+            this.lblKONUS.AutoSize = true;
+            this.lblKONUS.Location = new System.Drawing.Point(68, 132);
+            this.lblKONUS.Name = "lblKONUS";
+            this.lblKONUS.Size = new System.Drawing.Size(0, 17);
+            this.lblKONUS.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Canlı Konuşma";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKONUS);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKONUS;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Voice Applicaiton/Voice Applicaiton/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace VoiceApplicaion
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer speech = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Choices choices = new Choices();
+            string[] text = File.ReadAllLines(Environment.CurrentDirectory + "//grammar.text");
+            choices.Add(text);
+            Grammar grammar = new Grammar(new GrammarBuilder(choices));
+            recEngine.LoadGrammar(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            recEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recEngine_SpeechRecongnized);
+            speech.SelectVoiceByHints(VoiceGender.Male);
+        }
+
+        private void recEngine_SpeechRecongnized(object sender, SpeechRecognizedEventArgs e)
+        {
+            String result = e.Result.Text;
+
+            if (result == "Hello")
+            {
+                result = "Merhaba BEnim Adim Mete";
+
+            }
+
+            else if (result == "How are you")
+            {
+                result = "Sag ol Sen Nasilsin";
+
+            }
+
+            else if (result == "how old are you")
+            {
+                result = "1 yasindayim";
+
+            }
+
+            else if (result == "where do you live")
+            {
+                result = "Copmuter";
+
+            }
+
+
+            else if (result == "open Google")
+            {
+                result = "Googleyi acamiyorim";
+
+            }
+
+
+            speech.SpeakAsync(result);
+            lblKONUS.Text = result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
